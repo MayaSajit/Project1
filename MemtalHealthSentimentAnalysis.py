69d699f3526c4f0df36e1f2b1a6b61d46ac4b9df
MayaSajit <maya.sajit.nair@gmail.com>
2024-10-26 11:05:52 +0530

    Add files via upload

---
+++ MemtalHealthSentimentAnalysis.py
@@ -0,0 +1,25 @@
+
+import streamlit as st
+import pandas as pd
+from textblob import TextBlob
+df = pd.read_csv('/content/Tracking_Mental_Health.csv')
+column_name = 'Significant mental challenges'
+def custom_sentiment(text):
+  if text=='No Challenges':
+    return 1
+  elif text.lower()=='death of nearones' or text.lower()=='fake friendship' or text.lower()=="career anxiety" or text.lower()=='breakup' :
+    return -1
+  else:
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
+df['sentiment'] = df[column_name].apply(custom_sentiment)
+#df['Sentiment Category'] = df['sentiment'].apply(lambda x: 'Positive' if x > 0 else ('Negative' if x < 0 else 'Neutral'))
+overall_sentiment_score = df['sentiment'].mean()
+st.title("Sentiment Analysis App")
+st.write("Sentiment Scores for Significant mental challenges")
+if overall_sentiment_score >= 0.05:
+  st.write("Overall Sentiment: Positive 😊")
+elif overall_sentiment_score <= -0.05:
+  st.write("Overall Sentiment: Negative 😞")
+else:
+  st.write("Overall Sentiment: Neutral 😐")
